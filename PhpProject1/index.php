d2f8a4d0e294711920fcb7089a0205bb3d270f9a
samatha <samatha@samatha>
2013-05-18 16:29:56 +0530

    test1

--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -9,6 +9,7 @@
          echo "first commit from netBeans";
          echo "first commit from Git";
          echo "2nd commit from netBeans";
+         echo "3rd commit from netBeans";
         ?>
     </body>
 </html>
